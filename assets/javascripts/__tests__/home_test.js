8286a341254136c3c94c85c75c4d087f475ce66d
Teja <tejasrivastav@gmail.com>
2016-04-18 02:04:32 +0530

    Adds initial layout as per scketched mock-ups - Adds base scss and home page basic html

--- assets/javascripts/__tests__/home_test.js
+++ assets/javascripts/__tests__/home_test.js
@@ -9,18 +9,6 @@ var React     = require("react"),
 
 describe("HomePage", function () {
   it("changes the text after click", function () {
-    var homePage     = TestUtils.renderIntoDocument(
-          /* jshint ignore:start */
-          /* jscs ignore:start */
-          <HomePage labelOn="On" labelOff="Off" />
-          /* jshint ignore:end */
-          /* jscs ignore:end */
-        ),
-        checkboxNode = ReactDOM.findDOMNode(homePage).querySelector("label");
-    expect(checkboxNode.textContent).toEqual("Off");
-    TestUtils.Simulate.change(
-      TestUtils.findRenderedDOMComponentWithTag(homePage, "input")
-    );
-    expect(checkboxNode.textContent).toEqual("On");
+    // NOOP
   });
 });
--- assets/javascripts/components/templates/pages/home.jsx
+++ assets/javascripts/components/templates/pages/home.jsx
@@ -7,15 +7,95 @@ var Template = function (self) {
   return (
     /* jshint ignore:start */
     /* jscs ignore:start */
-    <div>
-      <h1>CBGA - BAT (Budget Analysis Tool)</h1>
-      <img src="images/cbga_logo.png" alt="CBGA Logo" />
-      <label>
-        <input type="checkbox"
-               checked={self.state.isChecked}
-               onChange={self.onChange} />
-        {self.state.isChecked ? self.props.labelOn : self.props.labelOff}
-      </label>
+    <div className="body">
+      <div className="bg-primary side-nav">
+        <div className="project-info">
+          <div className="project-title">CBGA Story Generator</div>
+          <div className="project-description">Some random generated text</div>
+        </div>
+        <div className="indicator">
+          <div className="search-box">
+            <input type="text" placeholder="Search for schemes or indicator" />
+          </div>
+          <div className="indicator-title">
+            <div className="indicator-title-text">CATEGORY</div>
+            <div className="clear-all"></div>
+          </div>
+          <div className="indicator-list">
+            <div className="indicator-list-item">
+              <div className="indicator-list-item-text">ABC</div>
+              <div className="indicator-list-item-selected"></div>
+            </div>
+          </div>
+        </div>
+        <div className="credits">
+          <div className="credits-info">
+            <span>@credits</span>
+            <span href="javascript:void(0)">DataKind Bangalore</span>
+          </div>
+          <div className="credits-links">
+            <span className="credits-links-item">Share</span>
+            <span className="credits-links-item">About</span>
+            <span className="credits-links-item">ContactUs</span>
+          </div>
+        </div>
+      </div>
+      <div className="content">
+        <div className="query-selector">
+          <div className="states">
+            <div className="states-header">
+              <div className="states-header-title">States</div>
+              <div className="btn btn-primary">Select State</div>
+            </div>
+            <div className="states-selected">
+              <div className="states-selected-active"></div>
+              <div className="states-selected-inactive">No states selected</div>
+            </div>
+          </div>
+        </div>
+        <div className="visual">
+          <div className="report">
+            <div className="report-header">
+              <div className="report-title">Union Budget Value for Year 2014</div>
+              <div className="report-subtitle"> select states to explore more</div>
+            </div>
+            <div className="report-body">
+              <div className="graph"></div>
+            </div>
+            <div className="report-footer">
+              <div className="report-footer-item">Download&nbsp;|&nbsp;</div>
+              <div className="report-footer-item">Embed&nbsp;|&nbsp;</div>
+              <div className="report-footer-item">Share</div>
+            </div>
+          </div>
+          <div className="meta">
+            <div className="meta-info">
+              <div className="meta-info-title">Title</div>
+              <div className="meta-info-unit">Unit</div>
+              <div className="meta-info-resource">Source</div>
+              <div className="meta-info-notes">Notes</div>
+            </div>
+          </div>
+        </div>
+        <div className="information-container">
+          <div className="information">
+            <div className="information-title">Budget Insights</div>
+            <div className="information-content">
+              Some random generated text
+              Some random generated text
+              Some random generated text
+            </div>
+          </div>
+          <div className="information">
+            <div className="information-title">Budget Insights</div>
+            <div className="information-content">
+              Some random genarated text
+              Some random genarated text
+              Some random genarated text
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     /* jshint ignore:end */
     /* jscs ignore:end */
